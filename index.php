269ea9ba7bbd88104983fcb3b013ee0dfd19d37f
choinseong <choinseong2237@gmail.com>
2024-02-25 22:16:38 +0900

    페이지 접속시 main페이지로 바로 갈 수 있도록 index.php에서 라우터 설정

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+$request = $_SERVER['REQUEST_URI'];
+$path = explode("?", $request);
+$path[1] = isset($path[1]) ? $path[1] : null;
+$resource = explode("/", $path[0]);
+// echo "<script>console.log('path[0] = " . $path[0] . "');</script>";
+// echo "<script>console.log('path[1] = " . $path[1] . "');</script>";
+
+$page = "";
+switch ($resource[1]) {
+    case '':
+        $page = "./pages/main.php";
+        break;
+}
+include($page);
+?>
