69d8fe54cd4ca5f4bc2b0b51d6213743ec6d631f
choinseong <choinseong2237@gmail.com>
2024-02-27 02:22:42 +0900

    리팩토링을 위한 작업

--- index.php
+++ index.php
@@ -6,19 +6,34 @@ $resource = explode("/", $path[0]);
 // echo "<script>console.log('path[0] = " . $path[0] . "');</script>";
 // echo "<script>console.log('path[1] = " . $path[1] . "');</script>";
 
-$page = "";
-switch ($resource[1]) {
-    case '':
-        $page = "./pages/main.php";
-        break;
-    case 'login':
-        $page = "./pages/login.php";
-        break;
-    case 'signup':
-        $page = "./pages/signup.php";
-        break;
-    default :
-        $page = "./pages/404.php";
-}
-include($page);
 ?>
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TOP10</title>
+    <link rel="stylesheet" href="../css/main.css">
+    <link rel="stylesheet" href="../css/login.css">
+    <link rel="stylesheet" href="../css/signup.css">
+</head>
+<body>
+    <?php
+    $page = "";
+    switch ($resource[1]) {
+        case '':
+            $page = "./pages/main.php";
+            break;
+        case 'login':
+            $page = "./pages/login.php";
+            break;
+        case 'signup':
+            $page = "./pages/signup.php";
+            break;
+        default :
+            $page = "./pages/404.php";
+    }
+    include($page);
+    ?>
+</body>
+</html>
